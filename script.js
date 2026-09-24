011be1307d5c95d8bcabf8675e5994c0bce4b15c
Mohamed-Malek268 <mohamedmalek.tanazefti@esprit.tn>
2026-09-24 09:51:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+//var age=20;
+//console.log(age);
+
+//console.log("bonjour".toUpperCase());
+
+//console.log("BONJOUR".toLowerCase());
+
+//console.log("bonjour".length);
+
+//console.log("tanazefti".indexOf("zef"))
+
+let note=15;
+if(note>=16){
+    console.log("tres bien");
+}else if(note>=14){
+    console.log("bien");
+
+}else if(note>=12){
+    console.log("assez bien");
+}else if(note>=10){
+    console.log("passable");
+}else{
+    console.log("nchlh 3am jeey");
+}
